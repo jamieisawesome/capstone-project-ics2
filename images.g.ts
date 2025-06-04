67a8dc2dab092f685678c20524957d6cc0cd1c17
jam !! <jamie.carter@student.csd509j.net>
2025-06-04 09:36:22 -0700

    sprite and tilemap added

---
+++ images.g.ts
@@ -0,0 +1,42 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "tommy":return img`
+. . . . e e e . . . . e e e 
+. . . . c e e c . . c e e c 
+. . . . c b e e f f e e b c 
+. . . . c 3 b e b e e b 3 c 
+. . . . f b 3 e e e e 3 b f 
+. . . . e e e e e e e e e e 
+b f b . e e f e e e e f e e 
+f e f . f e e f e e f e e f 
+f e f . f 2 e e b b e e b f 
+f e f f b b 2 2 2 2 2 2 f . 
+f b e b b e e e e e e b f . 
+. f f f e e b e e e e e f . 
+. . . f e f f e f f f e f . 
+. . . f f . . f f . . f f . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+// creating player sprite
+let myTommy = sprites.create(assets.image`tommy`, SpriteKind.Player)
+controller.moveSprite(myTommy)
+// tilemaps
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(myTommy)
