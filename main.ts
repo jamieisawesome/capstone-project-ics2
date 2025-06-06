3591bd5f60108db2dde7bf3ca740fc9a9e3e81a1
jam !! <jamie.carter@student.csd509j.net>
2025-06-06 08:29:52 -0700

    Update main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+// varibles
+let GRAVITY = 100
+let JUMPSPEED = -100
+let numberofjumps = 2
 // creating player sprite
-let myTommy = sprites.create(assets.image`tommy`, SpriteKind.Player)
-controller.moveSprite(myTommy)
+let myCat = sprites.create(assets.image`tommy`, SpriteKind.Player)
+controller.moveSprite(myCat, 100, 0)
+myCat.ay = GRAVITY
 // tilemaps
 tiles.setCurrentTilemap(tilemap`level1`)
-scene.cameraFollowSprite(myTommy)
+scene.cameraFollowSprite(myCat)
+tiles.placeOnTile(myCat, tiles.getTileLocation(2, 46))
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    myCat.vy = JUMPSPEED
+})
